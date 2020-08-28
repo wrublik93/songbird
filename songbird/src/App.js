2b1dae27dd13578290838845e0a568edd9606d49
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-08-28 11:53:15 +0300

    feat: add data for project

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+/* import Header from './components/Header/Header'; */
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <div className="wrapper">
+        {/* <Header /> */}
       </div>
     </div>
   );
