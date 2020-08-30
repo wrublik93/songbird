266c2b893b1232264d26d0aba12ff5118335516b
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-08-30 23:11:44 +0300

    style: add style for task

--- songbird/src/components/Game/Task/Task.jsx
+++ songbird/src/components/Game/Task/Task.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import taskImage from '../../../assets/images/common/task-image.jpg';
 import Player from '../../Player/Player';
 import './Task.scss';
 
@@ -6,11 +7,19 @@ function Task({
     correctAnswer,
     currentItem,
 }) {
-    const birdName = currentItem.name;
+    const birdName = correctAnswer ? currentItem.name : '******';
+    const birdImage = correctAnswer ? currentItem.image : taskImage;
+
+    const styleTask = `question title_song`
     return (
-        <div className="container">
-            <div>картинка</div>
-            <div>
+        <div className="task_container">
+            <div 
+                style={{ backgroundImage: `url(${birdImage})` }}
+                className="stub_picture"
+            >
+
+            </div>
+            <div className={styleTask}>
                 <p>{birdName}</p>
                 <Player 
                     audio = {currentItem.audio}
